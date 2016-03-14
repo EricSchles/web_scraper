8f75fc2273adc6937dc5f4e3b52d0b8ffa155526
Eric Schles <ericschles@gmail.com>
2016-03-13 21:00:26 -0400

    updating

--- app/models.py
+++ app/models.py
@@ -15,3 +15,14 @@ class HTML(db.Model):
         
     def __repr__(self):
         return "<html %r>" % self.id
+
+class ParsedHTML(db.Model):
+    __tablename__ = 'parsed_html'
+    id = db.Column(db.Integer,primary_key=True)
+    html = db.Column(db.String)
+    timestamp = db.Column(db.DateTime)
+    url = db.Column(db.String)
+    posting_body = db.Column(db.String)
+    title = db.Column(db.String)
+    img_urls = db.Column(db.String)
+    links = db.Column(db.String)
