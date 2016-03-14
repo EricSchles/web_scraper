fcdcabbb5b13d1db391dad6b3caee81d20c2e68a
Eric Schles <ericschles@gmail.com>
2016-03-13 21:15:30 -0400

    updating

--- app/ad_parser.py
+++ app/ad_parser.py
@@ -1,5 +1,6 @@
 import lxml.html
 from app.models import HTML
+import json
 
 def parse_title(html):
     html = lxml.html.fromstring(html)
@@ -7,4 +8,8 @@ def parse_title(html):
 
 def parse_posting_body(html):
     html = lxml.html.fromstring(html)
-    
+    return html.xpath('//div[@id="postingBody"]').text_content()
+
+def parse_links(html):
+    html = lxml.html.fromstring(html)
+    html.xpath('//div[@id="postingBody"]//a/@href')
