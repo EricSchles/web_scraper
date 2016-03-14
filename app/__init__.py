0ae62ea006228443c151372dee082b2d95f849a3
Eric Schles <ericschles@gmail.com>
2016-03-14 07:18:58 -0400

    updating

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.script import Manager
-from flask.migrate import Migrate, MigrateCommand
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
